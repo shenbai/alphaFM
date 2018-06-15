a528b077940cca3a938f164b0653bdccc1ffda58
shenbaise <shenbaise1001@126.com>
2018-06-15 16:23:20 +0800

    update

--- fm_train.cpp
+++ fm_train.cpp
@@ -3,84 +3,135 @@
 #include <fstream>
 #include "src/Frame/pc_frame.h"
 #include "src/FTRL/ftrl_trainer.h"
+#include "src/FTRL/ftrl_trainer_option.h"
+#include "src/Sgd/sgd_trainer.h"
+#include "src/Sgd/sgd_trainer_option.h"
 
 using namespace std;
 
-string train_help() 
-{
-    return string(
-        "\nusage: cat sample | ./fm_train [<options>]"
-        "\n"
-        "\n"
-        "options:\n"
-        "-m <model_path>: set the output model path\n"
-        "-s <space_size>: set space size, default 2^28\n"
-        "-compress <compress>: compress when output model, default 0\n"
-        "-dim <k0,k1,k2>: k0=use bias, k1=use 1-way interactions, k2=dim of 2-way interactions\tdefault:1,1,8\n"
-        "-init_stdev <stdev>: stdev for initialization of 2-way factors\tdefault:0.1\n"
-        "-w_alpha <w_alpha>: w is updated via FTRL, alpha is one of the learning rate parameters\tdefault:0.05\n"
-        "-w_beta <w_beta>: w is updated via FTRL, beta is one of the learning rate parameters\tdefault:1.0\n"
-        "-w_l1 <w_L1_reg>: L1 regularization parameter of w\tdefault:0.1\n"
-        "-w_l2 <w_L2_reg>: L2 regularization parameter of w\tdefault:5.0\n"
-        "-v_alpha <v_alpha>: v is updated via FTRL, alpha is one of the learning rate parameters\tdefault:0.05\n"
-        "-v_beta <v_beta>: v is updated via FTRL, beta is one of the learning rate parameters\tdefault:1.0\n"
-        "-v_l1 <v_L1_reg>: L1 regularization parameter of v\tdefault:0.1\n"
-        "-v_l2 <v_L2_reg>: L2 regularization parameter of v\tdefault:5.0\n"
-        "-core <threads_num>: set the number of threads\tdefault:1\n"
-        "-im <initial_model_path>: set the initial value of model\n"
-        "-fvs <force_v_sparse>: if fvs is 1, set vi = 0 whenever wi = 0\tdefault:0\n");
+string train_help() {
+	return string(
+			"\nusage: cat sample | ./fm_train [<options>]"
+					"\n"
+					"\n"
+					"options:\n"
+					"-opt <optimizer>: ftrl or sgd\n"
+					"-m <model_path>: set the output model path\n"
+					"-s <space_size>: set space size, default 2^28\n"
+					"-compress <compress>: compress when output model, default 0\n"
+					"-dim <k0,k1,k2>: k0=use bias, k1=use 1-way interactions, k2=dim of 2-way interactions\tdefault:1,1,8\n"
+					"-init_stdev <stdev>: stdev for initialization of 2-way factors\tdefault:0.1\n"
+					"-w_alpha <w_alpha>: w is updated via FTRL, alpha is one of the learning rate parameters\tdefault:0.05\n"
+					"-w_beta <w_beta>: w is updated via FTRL, beta is one of the learning rate parameters\tdefault:1.0\n"
+					"-w_l1 <w_L1_reg>: L1 regularization parameter of w\tdefault:0.1\n"
+					"-w_l2 <w_L2_reg>: L2 regularization parameter of w\tdefault:5.0\n"
+					"-v_alpha <v_alpha>: v is updated via FTRL, alpha is one of the learning rate parameters\tdefault:0.05\n"
+					"-v_beta <v_beta>: v is updated via FTRL, beta is one of the learning rate parameters\tdefault:1.0\n"
+					"-v_l1 <v_L1_reg>: L1 regularization parameter of v\tdefault:0.1\n"
+					"-v_l2 <v_L2_reg>: L2 regularization parameter of v\tdefault:5.0\n"
+					"-b_l1 <b_L1_reg>: L1 regularization parameter of w0\tdefault:0.1\n"
+					"-b_l2 <b_L2_reg>: L2 regularization parameter of w0\tdefault:5.0\n"
+					"-core <threads_num>: set the number of threads\tdefault:1\n"
+					"-im <initial_model_path>: set the initial value of model\n"
+					"-fvs <force_v_sparse>: if fvs is 1, set vi = 0 whenever wi = 0\tdefault:0\n");
 }
 
-vector<string> argv_to_args(int argc, char* argv[]) 
-{
-    vector<string> args;
-    for(int i = 1; i < argc; ++i)
-    {
-        args.push_back(string(argv[i]));
-    }
-    return args;
+vector<string> argv_to_args(int argc, char* argv[]) {
+	vector<string> args;
+	for (int i = 1; i < argc; ++i) {
+		args.push_back(string(argv[i]));
+	}
+	return args;
 }
 
+int get_opt(const vector<string>& args) {
+	int argc = args.size();
+	if (0 == argc)
+		throw invalid_argument("invalid command\n");
 
-int main(int argc, char* argv[])
-{
-    cin.sync_with_stdio(false);
-    cout.sync_with_stdio(false);
-    srand(time(NULL));
-    trainer_option opt;
-    try
-    {
-        opt.parse_option(argv_to_args(argc, argv));
-    }
-    catch(const invalid_argument& e)
-    {
-        cout << "invalid_argument:" << e.what() << endl;
-        cout << train_help() << endl;
-        return EXIT_FAILURE;
-    }
+	for (int i = 0; i < argc; ++i) {
+		if (args[i].compare("-opt") == 0) {
+			if (i == argc - 1)
+				throw invalid_argument("invalid command\n");
+			if (args[i] == "sgd") {
+				return 1;	// sgd
+			}
+		}
+	}
+	return 2;	// ftrl
+}
 
-    ftrl_trainer trainer(opt);
+int main(int argc, char* argv[]) {
+	cin.sync_with_stdio(false);
+	cout.sync_with_stdio(false);
+	srand(time(NULL));
 
-    if(opt.b_init) 
-    {
-        ifstream f_temp(opt.init_m_path.c_str());
-        if(!trainer.loadModel(f_temp)) 
-        {
-            cout << "wrong model" << endl;
-            return EXIT_FAILURE;
-        }
-        f_temp.close();
-    }
+	vector<string> args = argv_to_args(argc, argv);
+	int algo = 2;
+	algo = get_opt(args);
 
-    pc_frame frame;
-    frame.init(trainer, opt.threads_num);
-    frame.run();
+	if (algo == 1)	//sgd
+			{
+		sgd_trainer_option opt;
+		try {
+			opt.parse_option(argv_to_args(argc, argv));
+		} catch (const invalid_argument& e) {
+			cout << "invalid_argument:" << e.what() << endl;
+			cout << train_help() << endl;
+			return EXIT_FAILURE;
+		}
 
+		sgd_trainer trainer(opt);
 
-    ofstream f_model(opt.model_path.c_str(), ofstream::out);
-    trainer.outputModel(f_model);
-    f_model.close();
+		if (opt.b_init) {
+			ifstream f_temp(opt.init_m_path.c_str());
+			if (!trainer.loadModel(f_temp)) {
+				cout << "wrong model" << endl;
+				return EXIT_FAILURE;
+			}
+			f_temp.close();
+		}
 
-    return 0;
+		pc_frame frame;
+		frame.init(trainer, opt.threads_num);
+		frame.run();
+
+		ofstream f_model(opt.model_path.c_str(), ofstream::out);
+		trainer.outputModel(f_model);
+		f_model.close();
+
+		return 0;
+	} else {	// ftrl
+		ftrl_trainer_option opt;
+		try {
+			opt.parse_option(argv_to_args(argc, argv));
+		} catch (const invalid_argument& e) {
+			cout << "invalid_argument:" << e.what() << endl;
+			cout << train_help() << endl;
+			return EXIT_FAILURE;
+		}
+
+		ftrl_trainer trainer(opt);
+
+		if (opt.b_init) {
+			ifstream f_temp(opt.init_m_path.c_str());
+			if (!trainer.loadModel(f_temp)) {
+				cout << "wrong model" << endl;
+				return EXIT_FAILURE;
+			}
+			f_temp.close();
+		}
+
+		pc_frame frame;
+		frame.init(trainer, opt.threads_num);
+		frame.run();
+
+		ofstream f_model(opt.model_path.c_str(), ofstream::out);
+		trainer.outputModel(f_model);
+		f_model.close();
+
+		return 0;
+	}
+
 }
 
--- src/FTRL/ftrl_trainer.h
+++ src/FTRL/ftrl_trainer.h
@@ -6,142 +6,10 @@
 #include "ftrl_model.h"
 #include "../Sample/fm_sample.h"
 #include "../Utils/utils.h"
+#include "ftrl_trainer_option.h"
 #include <mutex>
 
 using namespace std;
-struct trainer_option
-{
-    trainer_option() : k0(true), k1(true), factor_num(8), init_mean(0.0), init_stdev(0.1), w_alpha(0.05), w_beta(1.0), w_l1(0.1), w_l2(5.0),
-               v_alpha(0.05), v_beta(1.0), v_l1(0.1), v_l2(5.0), 
-               threads_num(1), b_init(false), force_v_sparse(false),space_size(pow(2,28)), compress(false) {}
-    string model_path, init_m_path;
-    double init_mean, init_stdev;
-    double w_alpha, w_beta, w_l1, w_l2;
-    double v_alpha, v_beta, v_l1, v_l2;
-    int threads_num, factor_num, space_size;
-    bool k0, k1, b_init, force_v_sparse, compress;
-    
-    void parse_option(const vector<string>& args) 
-    {
-        int argc = args.size();
-        if(0 == argc) throw invalid_argument("invalid command\n");
-        for(int i = 0; i < argc; ++i)
-        {
-            if(args[i].compare("-m") == 0) 
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -m\n");
-                model_path = args[++i];
-            }
-            else if (args[i].compare("-s") == 0)
-            {
-                if (i == argc - 1)
-                    throw invalid_argument("invalid command -s\n");
-                space_size = stoi(args[++i]);
-            }
-            else if(args[i].compare("-dim") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -dim\n");
-                vector<string> strVec;
-                string tmpStr = args[++i];
-                utils::splitString(tmpStr, ',', &strVec);
-                if(strVec.size() != 3)
-                    throw invalid_argument("invalid command -dim ~\n");
-                k0 = 0 == stoi(strVec[0]) ? false : true;
-                k1 = 0 == stoi(strVec[1]) ? false : true;
-                factor_num = stoi(strVec[2]);
-            }
-            else if(args[i].compare("-init_stdev") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -init_stdev\n");
-                init_stdev = stod(args[++i]);
-            }
-            else if(args[i].compare("-w_alpha") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -w_alpha\n");
-                w_alpha = stod(args[++i]);
-            }
-            else if(args[i].compare("-w_beta") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -w_beta\n");
-                w_beta = stod(args[++i]);
-            }
-            else if(args[i].compare("-w_l1") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -w_l1\n");
-                w_l1 = stod(args[++i]);
-            }
-            else if(args[i].compare("-w_l2") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -w_l2\n");
-                w_l2 = stod(args[++i]);
-            }
-            else if(args[i].compare("-v_alpha") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -v_alpha\n");
-                v_alpha = stod(args[++i]);
-            }
-            else if(args[i].compare("-v_beta") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -v_beta\n");
-                v_beta = stod(args[++i]);
-            }
-            else if(args[i].compare("-v_l1") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -v_l1\n");
-                v_l1 = stod(args[++i]);
-            }
-            else if(args[i].compare("-v_l2") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -v_l1\n");
-                v_l2 = stod(args[++i]);
-            }
-            else if(args[i].compare("-core") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -core\n");
-                threads_num = stoi(args[++i]);
-            }
-            else if(args[i].compare("-im") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -im\n");
-                init_m_path = args[++i];
-                b_init = true; //if im field exits , that means b_init = true !
-            }
-            else if(args[i].compare("-fvs") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -fvs\n");
-                int fvs = stoi(args[++i]);
-                force_v_sparse = (1 == fvs) ? true : false;
-            }
-            else if(args[i].compare("-compress") == 0)
-            {
-                if(i == argc - 1)
-                    throw invalid_argument("invalid command -compress\n");
-                int c = stoi(args[++i]);
-                compress = (1 == compress) ? true : false;
-            }
-            else
-            {
-                throw invalid_argument("invalid command ~~\n");
-                break;
-            }
-        }
-    }
-
-};
 
 
 int num;
@@ -151,7 +19,7 @@ mutex mtx;
 class ftrl_trainer : public pc_task
 {
 public:
-    ftrl_trainer(const trainer_option& opt);
+    ftrl_trainer(const ftrl_trainer_option& opt);
     virtual void run_task(vector<string>& dataBuffer);
     bool loadModel(ifstream& in);
     void outputModel(ofstream& out);
@@ -168,7 +36,7 @@ private:
 };
 
 
-ftrl_trainer::ftrl_trainer(const trainer_option& opt)
+ftrl_trainer::ftrl_trainer(const ftrl_trainer_option& opt)
 {
     w_alpha = opt.w_alpha;
     w_beta = opt.w_beta;
