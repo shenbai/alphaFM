b7036aa9e9edb96301a40f217412a49849551794
shenbaise <shenbaise1001@126.com>
2019-03-07 19:30:20 +0800

    class_weight

--- src/Sample/fm_sample.h
+++ src/Sample/fm_sample.h
@@ -13,7 +13,7 @@ const string innerSpliter = ":";
 class fm_sample
 {
 public:
-    int y;
+    float y;
     vector<pair<int, double> > x;
     fm_sample(const string& line);
 };
@@ -26,6 +26,19 @@ fm_sample::fm_sample(const string& line)
     size_t pose = line.find_first_of(spliter, posb);
     int label = atoi(line.substr(posb, pose-posb).c_str());
     this->y = label > 0 ? 1 : -1;
+    if (label<=0){
+      this->y = -1;
+    } else if(label == 1){
+      this->y = 1;
+    } else if(label == 2){
+      this->y = 1.2;
+    } else if(label == 3){
+      this->y = 1.5;
+    } else if(label == 4){
+      this->y = 2;
+    } else{
+      this->y = 1;
+    }
     int key;
     double value;
     while(pose < line.size())
